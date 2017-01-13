7ff9ae61c93d5a9136e6257e266310b3e115d8bf
Brian Lewis <blewis8958@gmail.com>
2017-01-13 13:53:22 -0500

    prints 1-100 and at 5 it prints Gabe Newell

--- kata.rb
+++ kata.rb
@@ -1,5 +1,25 @@
+#using times method
+
 x = 0
 
-100.times do
-	puts x+=1
+100.times do 
+	if x == 4
+		puts "Gabe Newell"
+		x+=1
+	else
+		puts x+=1
+	end
 end
+
+
+
+#using Range method
+
+# (1..100).each do |num|
+# 	if num == 5
+# 		puts "Gabe Newell"
+# 	else
+# 	puts num
+# 	end
+# end
+
