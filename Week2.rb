b85057e5df8c67b412dd97eb3bc465c94895a1fc
Brian Lewis <blewis8958@gmail.com>
2017-01-18 14:25:07 -0500

    using functions to finsih previous exercise

--- Week2.rb
+++ Week2.rb
@@ -9,5 +9,19 @@ def MinedMindsKata(p1)
 	else
 		p1
 	end
+
+end
+
+def Printtext(p1)
+
 	
+	if (p1 % 3) == 0 && (p1 % 5) == 0		#States that if the number assigned to x is both divisible by 3 and 5 enter this loop
+		puts "Mined Minds"				#prints Mined Minds
+	elsif (p1 % 3) == 0 				#If the 1 condition fails and x is divisible by just 3 it enters the loop
+		puts "Mined"		#prints Mined
+	elsif (p1 % 5) == 0		#if condition 1 and 2 fail and x is divisible by just 5 it enters the loop
+		puts "Minds"		#prints Minds
+	else
+		puts p1				#prints the numberr assigned to x
+	end
 end
--- test_Week2.rb
+++ test_Week2.rb
@@ -1,36 +1,46 @@
 require "minitest/autorun"
 require_relative "Week2.rb"
 
+x = 1
+
 class TestMinedMindsKata < Minitest::Test
-	def test_1_returns_1
-		assert_equal(1,1)
-	end
 
-	def test_function_returns_1
-		assert_equal(1,MinedMindsKata(1))
-	end
 
-	def test_function_returns_2
-		assert_equal(2,MinedMindsKata(2))
-	end
 
-	def test_function_returns_mined
-		assert_equal("Mined",MinedMindsKata(3))
-	end
+	
 
-	def test_funtcion_returns_minds
-		assert_equal("Minds",MinedMindsKata(5))
-	end
+		def test_1_returns_1
+			assert_equal(1,1)
+		end
 
-	def test_function_returns_minds
-		assert_equal("Mined Minds",MinedMindsKata(15))
-	end
+		def test_function_returns_1
+			assert_equal(1,MinedMindsKata(1))
+		end
 
-	def test_function_returns_0
-		assert_equal("Mined Minds",MinedMindsKata(0))
-	end
+		def test_function_returns_mined
+			assert_equal("Mined",MinedMindsKata(3))
+		end
 
-	def test_function_returns_100
-		assert_equal("Minds",MinedMindsKata(100))
-	end
+		def test_funtcion_returns_minds
+			assert_equal("Minds",MinedMindsKata(5))
+		end
+
+		def test_function_returns_minds
+			assert_equal("Mined Minds",MinedMindsKata(15))
+		end
+
+		def test_function_returns_100
+			assert_equal("Minds",MinedMindsKata(100))
+		end
+
+
+	
+end
+
+100.times do
+
+Printtext(x)
+
+x+=1
+
 end
