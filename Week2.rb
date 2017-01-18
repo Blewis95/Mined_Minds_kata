2a1eff805beb5fcf689b2b6777e6c66801a7ac1b
Brian Lewis <blewis8958@gmail.com>
2017-01-18 13:59:36 -0500

    added tests

--- Week2.rb
+++ Week2.rb
@@ -1,9 +1,13 @@
 def MinedMindsKata(p1)
-	if p1 == 3
+
+	if (p1 % 3) == 0 && (p1 % 5) == 0
+		"Mined Minds"
+	elsif (p1 % 3) == 0 
 		"Mined"
-	elsif p1 == 5
+	elsif (p1 % 5) == 0
 		"Minds"
 	else
 		p1
 	end
+	
 end
--- test_Week2.rb
+++ test_Week2.rb
@@ -21,4 +21,16 @@ class TestMinedMindsKata < Minitest::Test
 	def test_funtcion_returns_minds
 		assert_equal("Minds",MinedMindsKata(5))
 	end
+
+	def test_function_returns_minds
+		assert_equal("Mined Minds",MinedMindsKata(15))
+	end
+
+	def test_function_returns_0
+		assert_equal("Mined Minds",MinedMindsKata(0))
+	end
+
+	def test_function_returns_100
+		assert_equal("Minds",MinedMindsKata(100))
+	end
 end
